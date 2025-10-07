b46f36ee3c8557b5926fe5c9bfd1f39daf96bc6f
hrzr0599 <a.23308051280599@cbtis128.edu.mx>
2025-10-07 13:12:12 -0600

    Delete UII Act 5 Tabla Hernandez Roman directory

--- UII Act 5 Tabla Hernandez Roman/app_Productos/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Producto
-
-# Create your views here.
-
-def index(request):
-    productos=Producto.objects.all()
-    return render(request, 'index.html', {'productos':productos})
